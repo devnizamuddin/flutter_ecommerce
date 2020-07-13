22d6b1c06459cad137e0e52a4bbeee102f722c5a
nshamrat7 <nshamrat7@gmail.com>
2020-07-13 21:43:37 +0600

    three

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
+
   runApp(MaterialApp(
-      home: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: <Widget>[
-      Text('New World'),
-      Text('Hello World'),
-      Text('Hello World')
-    ],
-  )));
+
+    home: ListView(
+      children: <Widget>[
+        Text('One'),
+        Text('One'),
+        Text('One'),
+        Text('One'),
+        Text('One'),
+        Text('One'),
+        Text('One'),
+        Text('One'),
+        Text('One'),
+        Text('One'),
+        Text('One'),
+        Text('One'),
+        Text('One'),
+
+      ],
+    ),
+
+  ));
+
 }
